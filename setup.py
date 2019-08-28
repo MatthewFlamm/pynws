7f47630c0ec10c2f2beb73bb40ea38197f6c3e49
Flamm, Matthew H <matthewhflamm@gmail.com>
2019-08-28 11:56:20 +0100

    remove py3.5

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
     ],
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7"]
 )
