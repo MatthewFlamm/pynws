bcc93bc10c11753636d5e3d78a8c6e2de50f2043
Lyman Epp <lyman@epptech.com>
2022-03-13 11:49:25 -0400

    Merge debugging logic into raw_stations_observations_latest

--- pynws/raw_data.py
+++ pynws/raw_data.py
@@ -47,10 +47,7 @@ async def raw_stations_observations_latest(station, websession, userid):
     """Get observation response from station"""
     url = pynws.urls.stations_observations_latest_url(station)
     header = get_header(userid)
-    async with websession.get(url, headers=header) as res:
-        res.raise_for_status()
-        obs = await res.json()
-    return obs
+    return await _make_request(websession, url, header)
 
 
 async def raw_points_stations(lat, lon, websession, userid):
