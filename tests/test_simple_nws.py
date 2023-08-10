f341ba16aba56371a600d26df2753ad8a0755bc6
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2023-08-10 17:36:21 +0000

    fix null values

--- tests/test_simple_nws.py
+++ tests/test_simple_nws.py
@@ -174,7 +174,7 @@ async def test_nws_forecast(aiohttp_client, event_loop, mock_urls):
     assert forecast[0]["windBearing"] == 180
 
     # tests null value in quatitative value
-    assert forecast[1]["probabilityOfPrecipitation"] == 0
+    assert forecast[1]["probabilityOfPrecipitation"] is None
 
 
 @freeze_time("2019-10-14T21:30:00-04:00")
