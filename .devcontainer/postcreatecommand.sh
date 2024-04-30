069e963e5912f49a76f8892ab1876c637816a215
Lyman Epp <lyman@epptech.com>
2024-04-30 13:19:11 -0400

    Use ruff for vscode linting Fix failed postcreatecommand.sh script (Windows only?)

--- .devcontainer/postcreatecommand.sh
+++ .devcontainer/postcreatecommand.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+git config --global --add safe.directory "$PWD"
+
 python -m pip install -e .
 python -m pip install -r requirements-test.txt
 python -m pip install pre-commit
