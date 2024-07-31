330d61828f714f6a6ffd008833721cbd12a4a2ac
makushimu <makushimu@starlite>
2024-07-31 11:28:06 -0400

    Remove auto from StrEnum

--- src/pynws/forecast_units.py
+++ src/pynws/forecast_units.py
@@ -1,13 +1,13 @@
 import sys
 
 if sys.version_info >= (3, 11):
-    from enum import StrEnum, auto
+    from enum import StrEnum
 else:
-    from .backports.enum import StrEnum, auto
+    from .backports.enum import StrEnum
 
 
 class NwsForecastUnits(StrEnum):
     """Values accepted as forecast_units."""
 
-    US = auto()
-    SI = auto()
+    US = "us"
+    SI = "si"
